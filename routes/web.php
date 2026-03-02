45a2aa896691ec7871d8a7fc1dad3b29cf2ba6ee
Alvaro Colunga <alvaro.colunga@gmail.com>
2026-03-02 16:40:06 -0600

    feat: se agrego middleware para rutas de admin y user

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,9 @@ Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::middleware('auth')->group(function () {
     Route::get('/home', [HomeController::class, 'index'])->name('home');
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+});
 
+Route::middleware(['auth', 'user_type:admin'])->group(function () {
     Route::get('/categorias', [CategoriasController::class, 'index'])->name('categorias.index');
     Route::get('/categorias/create', [CategoriasController::class, 'create'])->name('categorias.create');
     Route::post('/categorias/store', [CategoriasController::class, 'store'])->name('categorias.store');
@@ -30,5 +32,9 @@ Route::middleware('auth')->group(function () {
     Route::post('/libros/store', [LibrosController::class, 'store'])->name('libros.store');
     Route::get('/libros/{id}/edit', [LibrosController::class, 'edit'])->name('libros.edit');
     Route::put('/libros/{id}', [LibrosController::class, 'update'])->name('libros.update');
-    Route::delete('/libros/{id}', [LibrosController::class, 'destroy'])->name('libros.destroy');
+    Route::delete('/libros/{id}', [LibrosController::class, 'destroy'])->name('libros.destroy');    
+});
+
+Route::middleware(['auth', 'user_type:user'])->group(function () {
+    
 });
